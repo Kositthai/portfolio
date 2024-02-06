dac775e98a8486c0c9ea47b50d3d45727bf73401
Kositthai <vkositthai@outlook.com>
2024-02-06 23:55:29 +0100

    Update aboutme

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,13 +1,13 @@
-import styled from 'styled-components'
-import {useEffect, useRef} from 'react'
-import mySelf from '../assets/images/IMG_1500-blur-removebg-preview.png'
-import bike from '../assets/images/bicycle.png'
-import hyf from '../assets/images/hyf-logo.svg'
-import walor from '../assets/images/walor-logo.svg'
-import Skills from '../components/Skills'
-import Certificate from '../components/Certificate'
-import {experienceAnimation} from '../animations/experienceAnimation'
-import ExperienceBar from '../components/ExperienceBar'
+import styled from 'styled-components';
+import { useEffect, useRef } from 'react';
+import mySelf from '../assets/images/IMG_1500-blur-removebg-preview.png';
+import bike from '../assets/images/bicycle.png';
+import hyf from '../assets/images/hyf-logo.svg';
+import walor from '../assets/images/walor-logo.svg';
+import Skills from '../components/Skills';
+import Certificate from '../components/Certificate';
+import { experienceAnimation } from '../animations/experienceAnimation';
+import ExperienceBar from '../components/ExperienceBar';
 
 const Container = styled.div`
   width: 100%;
@@ -17,12 +17,12 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   margin: auto;
-`
+`;
 
 const Wrapper = styled.div`
   width: 80%;
   height: auto;
-  border: 3px solid ${({theme}) => theme.primary};
+  border: 3px solid ${({ theme }) => theme.primary};
   border-radius: 5px;
   position: relative;
   margin: 5rem 0;
@@ -37,14 +37,14 @@ const Wrapper = styled.div`
   @media (max-width: 1024px) {
     width: 95%;
   }
-`
+`;
 
 const Frame = styled.div`
   border-radius: 50%;
   width: 15rem;
   border: none;
   margin: auto 2rem;
-  background: ${({theme}) => theme.orange};
+  background: ${({ theme }) => theme.orange};
   overflow: hidden;
 
   @media (max-width: 1024px) {
@@ -53,7 +53,7 @@ const Frame = styled.div`
     margin: 1;
     background: transparent;
   }
-`
+`;
 
 const Image = styled.img`
   width: 100%;
@@ -63,17 +63,18 @@ const Image = styled.img`
   @media (max-width: 1024px) {
     display: none;
   }
-`
+`;
 
 const IntroduceMessage = styled.div`
   width: fit-content;
-  border: 2px solid ${({theme}) => theme.primary};
+  border: 2px solid ${({ theme }) => theme.primary};
   border-radius: 1rem;
   margin: 3rem 3rem 3rem 0;
   font-size: 1.2rem;
   padding: 1rem;
   line-height: 1.5;
   position: relative;
+  font-weight: 500;
 
   p {
     margin: 0.5rem 0;
@@ -89,7 +90,7 @@ const IntroduceMessage = styled.div`
     margin: 1rem;
     border: none;
   }
-`
+`;
 
 const ResumeContainer = styled.div`
   display: flex;
@@ -97,11 +98,11 @@ const ResumeContainer = styled.div`
   @media (max-width: 1024px) {
     display: block;
   }
-`
+`;
 
 const StlyedBike = styled.img`
   width: 7rem;
-`
+`;
 
 const StyledHyf = styled.img`
   width: 3rem;
@@ -112,30 +113,30 @@ const StyledHyf = styled.img`
   @media (max-width: 1280px) {
     left: 27%;
   }
-`
+`;
 
 const StyledHyfMessage = styled.p`
-  color: ${({theme}) => theme.primary};
+  color: ${({ theme }) => theme.primary};
   font-weight: 600;
   position: absolute;
   left: 28%;
   opacity: 0;
-`
+`;
 
 const StyledWalor = styled.img`
   width: 4rem;
   position: absolute;
   left: 63%;
   opacity: 0;
-`
+`;
 
 const StyledWalorMessage = styled.p`
-  color: ${({theme}) => theme.primary};
+  color: ${({ theme }) => theme.primary};
   font-weight: 600;
   position: absolute;
   left: 65%;
   opacity: 0;
-`
+`;
 
 const Experience = styled.p`
   font-size: 1.5rem;
@@ -147,49 +148,49 @@ const Experience = styled.p`
     font-size: 1rem;
     margin-left: 1rem;
   }
-`
+`;
 
-const ExperienceContainer = styled.div``
+const ExperienceContainer = styled.div``;
 
 const ExperienceTitle = styled.h2`
   font-size: 1rem;
   margin: 1rem;
-`
+`;
 
 const StyledImage = styled.img`
   width: 20%;
   float: left;
-`
+`;
 
 const ExperienceContent = styled.div`
   display: flex;
   justify-content: center;
-`
+`;
 
 const AnimationContainer = styled.div`
   position: relative;
   width: 100%;
-`
+`;
 
 const AboutMe: React.FC = () => {
-  const myRef = useRef<HTMLDivElement>(null)
+  const myRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const {restartExperienceAnimation} = experienceAnimation()
+    const { restartExperienceAnimation } = experienceAnimation();
 
     const observer = new IntersectionObserver((entries) => {
-      const entry = entries[0]
+      const entry = entries[0];
 
       if (entry.isIntersecting) {
-        restartExperienceAnimation()
+        restartExperienceAnimation();
       }
-    })
+    });
 
     // Observe the element
     if (myRef.current) {
-      observer.observe(myRef.current)
+      observer.observe(myRef.current);
     }
-  }, [myRef])
+  }, [myRef]);
 
   return (
     <Container id="about">
@@ -204,21 +205,27 @@ const AboutMe: React.FC = () => {
             )}
 
             <div>
-              <p>
-                Hello, I'm <strong>Vipavee Kositthai</strong>, Background of
-                Logistics management. I discovered my new passion for coding
-                during my unemployment. This discovery led my journey further by
-                joining the <strong>Hack Your Future Bootcamp.</strong> {''}
-                After successfully completing the bootcamp, I had the fantastic
-                opportunity to join <strong>Walor.</strong>
-              </p>
-
-              <p>
-                In my role as a <strong>front-end developer</strong>, I am
-                responsible for implementing and updating features to enhance
-                the user interface, with a focus on optimizing the overall user
-                experience.
-              </p>
+              Hello, I'm <strong>Vipavee Kositthai</strong>, with a{' '}
+              <strong>background in Logistics Management</strong>. My journey
+              into coding began during a period of unemployment, where I
+              discovered a new passion. This discovery led me to join the{' '}
+              <strong>Hack Your Future Bootcamp</strong>, an experience that
+              opened up new opportunities, including my current role at{' '}
+              <strong>Walor.</strong>
+              <br></br>
+              As a <strong>front-end developer</strong>, my responsibilities
+              include implementing and updating features to improve the user
+              interface, with a keen focus on enhancing the overall user
+              experience.
+              <br></br>Recently, I've <strong>transitioned</strong> into backend
+              development, working with <strong>PHP and Laravel</strong>. This
+              transition not only <strong>enhances my skills</strong> by
+              combining frontend and backend knowledge but also allows me to
+              tackle projects with a more comprehensive perspective, ensuring
+              <strong>seamless integration and robust functionality</strong>.
+              <br></br>
+              I'm excited to <strong>continue learning</strong> and expanding my
+              skills in new technologies.
             </div>
           </IntroduceMessage>
         </ResumeContainer>
@@ -246,7 +253,7 @@ const AboutMe: React.FC = () => {
         )}
       </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default AboutMe
+export default AboutMe;
